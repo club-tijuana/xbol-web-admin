171594de02fcde4fac8e81fd839e388fe329b163
Roberto Galvan <roberto.galvan@odasoft.com.mx>
2026-04-01 16:43:35 -0700

    Feature xbol 545 support (#45)
    * feature(Support): Add base layouts.
    
    * chore(Support): Set logic to get order info.
    
    * feat(Support): Update client information.
    
    * chore(Support): Clean up
    
    * chore(OrderActions): Fix UI order actions and call dialog

---
+++ Odasoft.XBOL.AdminPortal/Odasoft.XBOL.Common/Constants/HttpStatusCodes.cs
@@ -0,0 +1,9 @@
+namespace Odasoft.XBOL.Common.Constants
+{
+    public static class HttpStatusCodes
+    {
+        public const int NO_CONTENT = 204;
+
+        public const int NOT_FOUND = 404;
+    }
+}
